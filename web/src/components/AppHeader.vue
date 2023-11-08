9aa80b8c2caa99c36a516691b3a768b2f7070f23
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-11-09 00:37:35 +0800

    ✨ feat: 使用iconify图标Logo

--- web/src/components/AppHeader.vue
+++ web/src/components/AppHeader.vue
@@ -7,33 +7,34 @@ const changeLang = () => {
 
 <template>
   <header class="bg-nav">
-    <Container  />
-    <el-menu mode="horizontal" :ellipsis="false" class="!bg-transparent">
-      <el-menu-item index="0" class="!inline-flex dark:!hidden">
-        <img class="h-8" src="@/assets/images/go-next-logo.svg" alt="Go Next Logo"/>
-        <span class="ml-1 text-2xl">GONEXT</span>
-      </el-menu-item>
-      <div class="flex-grow"/>
-      <el-sub-menu index="1">
-        <template #title>
-          <i class="i-ri:earth-line text-2xl"/>
-        </template>
-        <el-menu-item index="2-1">English</el-menu-item>
-        <el-menu-item index="2-2">简体中文</el-menu-item>
-      </el-sub-menu>
-      <el-menu-item index="2">
-        <template #title>
-          <i class="i-ri:github-fill text-2xl"/>
-        </template>
-      </el-menu-item>
-      <el-menu-item index="3">登录/注册</el-menu-item>
-    </el-menu>
+    <Container :fluid="false">
+      <el-menu mode="horizontal" :ellipsis="false" class="!bg-transparent !border-b-0">
+        <el-menu-item index="0" class="!inline-flex dark:!hidden !text-red">
+          <i class="i-fa:dashcube text-4xl"/>
+<!--          <span class="ml-0.5 text-2xl"><span class="text-black dark:text-white">Go</span>Next</span>-->
+        </el-menu-item>
+        <div class="flex-grow"/>
+        <el-sub-menu index="1">
+          <template #title>
+            <i class="i-ri:earth-line text-2xl"/>
+          </template>
+          <el-menu-item index="2-1">English</el-menu-item>
+          <el-menu-item index="2-2">简体中文</el-menu-item>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <template #title>
+            <i class="i-ri:github-fill text-2xl"/>
+          </template>
+        </el-menu-item>
+        <el-menu-item index="3">登录/注册</el-menu-item>
+      </el-menu>
+    </Container>
   </header>
 </template>
 
 <style scoped lang="scss">
 .bg-nav {
-  @apply fixed w-full z-50 bg-[length:4px_4px] bg-transparent;
+  @apply sticky w-full z-50 bg-[length:4px_4px] bg-transparent border-b-1 top-0;
   backdrop-filter: saturate(50%) blur(4px);
   background-image: radial-gradient(transparent 1px, white 1px);
 }
