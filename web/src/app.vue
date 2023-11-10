91a683b50557585db45a17ca0c676e9f0141fe41
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-11-10 10:20:42 +0800

    ✨ feat: element支持中文

--- web/src/app.vue
+++ web/src/app.vue
@@ -1,7 +1,21 @@
 <template>
-  <NuxtLayout>
-    <NuxtLoadingIndicator/>
-    <NuxtPage/>
-    <ElBacktop/>
-  </NuxtLayout>
+  <el-config-provider :locale="zh">
+    <NuxtLayout>
+      <NuxtLoadingIndicator/>
+      <NuxtPage/>
+      <ElBacktop/>
+    </NuxtLayout>
+  </el-config-provider>
 </template>
+<script setup lang="ts">
+// import {useI18n} from 'vue-i18n'
+import zh from 'element-plus/es/locale/lang/zh-cn'
+// import en from 'element-plus/es/locale/lang/en'
+
+// let locale11 = ref(en)
+// const {t, locale} = useI18n({useScope: 'global'})
+// const switchLang = () => {
+//   locale11 = zh
+//   locale.value = 'zh'
+// }
+</script>
