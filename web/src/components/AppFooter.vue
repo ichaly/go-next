f4a143739ea88f65f18887fa5df4bf7e6daff142
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-11-17 22:15:55 +0800

    ✨ feat: 简单实现页脚

--- web/src/components/AppFooter.vue
+++ web/src/components/AppFooter.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <div></div>
+  <div class="border-t-1 py-8">
+    <container>
+      <div class="text-gray-500 text-sm flex flex-row items-center justify-between">
+        <div>Released under the MIT License.</div>
+        <div>Copyright © 2023 iChaly.</div>
+      </div>
+    </container>
+  </div>
 </template>
 
 <style scoped lang="ts">
