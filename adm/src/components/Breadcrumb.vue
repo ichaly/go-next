663f58e09edada82c64012c031984d3370b9000f
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-12-13 17:55:15 +0800

    ✨ feat: 完成面包屑导航

--- adm/src/components/Breadcrumb.vue
+++ adm/src/components/Breadcrumb.vue
@@ -1,30 +1,30 @@
 <template>
-  <el-breadcrumb class="hidden md:block" :separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }" v-for="item in matched">
-      {{ item.meta.title }}
+  <el-breadcrumb class='hidden md:block' :separator-icon='ArrowRight'>
+    <el-breadcrumb-item v-for='item in matched'>
+      {{ item.title }}
     </el-breadcrumb-item>
-    <!--    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>-->
-    <!--    <el-breadcrumb-item>-->
-    <!--      <router-link to="/adm/public">用户管理</router-link>-->
-    <!--    </el-breadcrumb-item>-->
-    <!--    <el-breadcrumb-item>用户详情</el-breadcrumb-item>-->
   </el-breadcrumb>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import ArrowRight from '@/components/ArrowRight.vue'
 
-// const route = useRoute()
-// const { menus } = toRefs(useRootStore())
-// const parse = (items: Item[]) => {
-//   for (let item of items) {
-//     if (item.children) {
-//       return [...parse(item.children)]
-//     } else {
-//       if (item.name === route.path) {
-//         return [{ path: item.name, title: item.title }]
-//       }
-//     }
-//   }
-// }
+const route = useRoute()
+const { menus } = toRefs(useRootStore())
+const search = (items: Item[]): Item[] => {
+  for (let item of items) {
+    if (`/${item.name}` === route.path) {
+      return [item]
+    } else if (item.children?.length) {
+      let nodes = search(item.children)
+      if (nodes.length) {
+        return [item, ...nodes]
+      }
+    }
+  }
+  return []
+}
+const matched = computed(() => {
+  return [{ title: '首页' }, ...search(menus.value)]
+})
 </script>
