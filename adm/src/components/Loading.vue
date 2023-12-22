0e15420a4c7b66cd6158911828a40b932a898953
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-12-22 17:55:25 +0800

    ✨ feat: 统一首屏和懒加载页面loading

--- adm/src/components/Loading.vue
+++ adm/src/components/Loading.vue
@@ -1,3 +1,59 @@
 <template>
-  <div v-loading="true" class="w-full h-full" element-loading-text="正在加载中"></div>
+  <div class='w-full h-full flex flex-col center'>
+    <div class='loader'></div>
+    <div class='load_title'>正在努力加载中</div>
+  </div>
 </template>
+<style scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.loader {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  position: relative;
+  border: 3px solid transparent;
+  border-top-color: #409eff;
+  animation: spin 2s linear infinite;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #409eff;
+    animation: spin 2s linear infinite;
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    right: 15px;
+    bottom: 15px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #409eff;
+    animation: spin 2s linear infinite;
+  }
+}
+
+.load_title {
+  margin-top: 30px;
+  color: #409eff;
+  font-size: 16px;
+  line-height: 30px;
+}
+</style>
