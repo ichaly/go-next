6f9f2c136da57afa8b8e1259ea1c7a893d9c65d9
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-12-23 01:03:26 +0800

    ✨ feat: 消除消除VueRouter使用defineAsyncComponent的警告

--- adm/src/composables/useLoadingComponent.ts
+++ adm/src/composables/useLoadingComponent.ts
@@ -2,8 +2,11 @@ import Loading from '@/components/Loading.vue'
 import type { AsyncComponentLoader } from 'vue'
 
 export function useLoadingComponent(loader: AsyncComponentLoader) {
-  return defineAsyncComponent({
+  const component = defineAsyncComponent({
     loader,
     loadingComponent: Loading
   })
+  //消除VueRouter的警告,参考https://github.com/vuejs/router/issues/1469#issuecomment-1458400428
+  component.__warnedDefineAsync = true
+  return component
 }
