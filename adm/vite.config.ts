eb322596ac68cd689697bc3ca097c481ae886e97
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-12-28 13:18:05 +0800

    🦄 refactor: IndexResolver支持优先级配置化

--- adm/vite.config.ts
+++ adm/vite.config.ts
@@ -14,16 +14,36 @@ import type { ComponentResolver } from 'unplugin-vue-components/types'
 
 export interface IndexResolverOptions {
   exclude?: RegExp,
+  sortFn?: (a: string, b: string) => number
 }
 
 function IndexResolver(options: IndexResolverOptions): ComponentResolver[] {
+  let optionsResolved: IndexResolverOptions
+
+  async function resolveOptions() {
+    if (optionsResolved)
+      return optionsResolved
+    const include: string[] = ['vue', 'tsx', 'jsx', 'ts', 'js']
+    optionsResolved = {
+      exclude: /(RouterLink|RouterView)$/,
+      sortFn: (a, b) => {
+        let aExt = a.slice(a.lastIndexOf('.') + 1)
+        let bExt = b.slice(b.lastIndexOf('.') + 1)
+        return include.indexOf(aExt) - include.indexOf(bExt)
+      },
+      ...options
+    }
+    return optionsResolved
+  }
+
   return [
     {
       type: 'component',
       resolve: async (name: string) => {
+        const options = await resolveOptions()
         if (options.exclude && name.match(options.exclude))
           return
-        let files = fg.globSync(`./src/components/${name}/index.{ts,js,vue}`)
+        const files = fg.globSync(`./src/components/${name}/index.{vue,ts,tsx,js,jsx}`).sort(options.sortFn)
         for (let file of files) {
           let from = fileURLToPath(new URL(file, import.meta.url))
           return { name: 'default', from }
