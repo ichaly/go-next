1bc5de807e5fe6d9ca341c23e3adfeadd3d7a1af
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-01-08 19:21:51 +0800

    🐞 fix: 屏蔽初始化找不到元素的异常

--- adm/src/components/Captcha.vue
+++ adm/src/components/Captcha.vue
@@ -6,8 +6,8 @@
 import { randomString } from '@/utils/strings'
 
 interface Props {
-  prefix?: string// 身份标。开通阿里云验证码2.0后，您可以在控制台概览页面的实例基本信息卡片区域，获取身份标
-  sceneId?: string// 场景ID。根据步骤二新建验证场景后，您可以在验证码场景列表，获取该场景的场景ID
+  prefix?: string // 身份标。开通阿里云验证码2.0后，您可以在控制台概览页面的实例基本信息卡片区域，获取身份标
+  sceneId?: string // 场景ID。根据步骤二新建验证场景后，您可以在验证码场景列表，获取该场景的场景ID
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -23,12 +23,12 @@ const MySlot = h(defaultSlot, { id })
 
 // 获取验证码验证结果和业务结果
 const captchaVerifyCallback = async (captchaVerifyParam: {
-  sceneId: string,
-  certifyId: string,
-  deviceToken: string,
-  data: string,
+  sceneId: string
+  certifyId: string
+  deviceToken: string
+  data: string
 }): Promise<{
-  captchaResult: boolean,
+  captchaResult: boolean
   bizResult: boolean
 }> => {
   // // 1.向后端发起业务请求，获取验证码验证结果和业务结果
@@ -50,16 +50,21 @@ const onBizResultCallback = (bizResult: boolean) => {
 
 const { locale } = useI18n()
 const initCaptcha = () => {
-  initAliyunCaptcha({
-    mode: 'popup',
-    button: `#${id}`,
-    prefix: props.prefix,
-    SceneId: props.sceneId,
-    captchaVerifyCallback: captchaVerifyCallback, // 业务请求(带验证码校验)回调函数，无需修改
-    onBizResultCallback: onBizResultCallback, // 业务请求结果回调函数，无需修改
-    slideStyle: { width: 360, height: 30 }, // 滑块验证码样式，支持自定义宽度和高度，单位为px。其中，width最小值为320 px
-    language: locale.value || 'cn' // 验证码语言类型，支持简体中文（cn）、繁体中文（tw）、英文（en）
-  })
+  try {
+    if (document.querySelector(`#${id}`)) {
+      initAliyunCaptcha({
+        mode: 'popup',
+        button: `#${id}`,
+        prefix: props.prefix,
+        SceneId: props.sceneId,
+        captchaVerifyCallback: captchaVerifyCallback, // 业务请求(带验证码校验)回调函数，无需修改
+        onBizResultCallback: onBizResultCallback, // 业务请求结果回调函数，无需修改
+        slideStyle: { width: 360, height: 30 }, // 滑块验证码样式，支持自定义宽度和高度，单位为px。其中，width最小值为320 px
+        language: locale.value || 'cn' // 验证码语言类型，支持简体中文（cn）、繁体中文（tw）、英文（en）
+      })
+    }
+  } catch (e) {
+  }
 }
 
 onMounted(() => {
