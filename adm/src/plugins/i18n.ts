cde838fc026ac309cc94c3e278b35cf1462de2c3
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-01-08 15:11:00 +0800

    🦄 refactor: 重命名目录

--- adm/src/plugins/i18n.ts
+++ adm/src/plugins/i18n.ts
@@ -3,9 +3,9 @@ import { createI18n } from 'vue-i18n'
 const storage = useStorage('lang', 'cn')
 
 const messages = Object.entries(
-  import.meta.glob<Record<string, unknown>>('/src/langs/*.ts', { eager: true })
+  import.meta.glob<Record<string, unknown>>('/src/locales/*.ts', { eager: true })
 ).reduce((result, [path, module]) => {
-  let key = path.match(/\/src\/langs\/(.*)\.ts$/)?.[1]
+  let key = path.match(/\/src\/locales\/(.*)\.ts$/)?.[1]
   return { ...result, [key!]: module.default }
 }, {})
 
