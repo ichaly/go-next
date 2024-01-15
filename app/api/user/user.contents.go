bc1873fe41ae91b5b8556cf753137fd4b460dbcb
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-01-15 20:59:06 +0800

    🦄 refactor: 优化删除多余逻辑判断

--- app/api/user/user.contents.go
+++ app/api/user/user.contents.go
@@ -45,21 +45,20 @@ func (my *contents) Resolve(p graphql.ResolveParams) (interface{}, error) {
 }
 
 func (my *contents) batchContents(ctx context.Context, keys []uint64) []*gql.Result[[]*cms.Content] {
+	//从数据库获取数据
 	var res []*cms.Content
 	err := my.db.WithContext(ctx).Model(&cms.Content{}).Where("created_by in ?", keys).Find(&res).Error
+	//分组数据
 	values := make(map[uint64][]*cms.Content)
 	for _, c := range res {
 		values[*c.CreatedBy] = append(values[*c.CreatedBy], c)
 	}
+	//填充结果集
 	results := make([]*gql.Result[[]*cms.Content], len(keys))
 	for i, k := range keys {
-		r := &gql.Result[[]*cms.Content]{
-			Error: err,
+		results[i] = &gql.Result[[]*cms.Content]{
+			Error: err, Data: values[k],
 		}
-		if v, ok := values[k]; ok {
-			r.Data = v
-		}
-		results[i] = r
 	}
 	return results
 }
