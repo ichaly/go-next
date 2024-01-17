2cef7731605e28eb23309c6e37d3f25628a04c43
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-01-17 16:03:22 +0800

    ✨ feat: 支持自动表注释

--- pkg/base/connect.go
+++ pkg/base/connect.go
@@ -2,10 +2,12 @@ package base
 
 import (
 	"fmt"
+	"github.com/ichaly/go-next/pkg/gql"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 	"time"
 )
 
@@ -24,9 +26,23 @@ func NewConnect(c *Config, p []gorm.Plugin, e []interface{}) (*gorm.DB, error) {
 		}
 	}
 	if c.App.Debug {
-		err = db.AutoMigrate(e...)
-		if err != nil {
-			return nil, err
+		for _, v := range e {
+			name, desc := "", ""
+			if n, ok := v.(schema.Tabler); ok {
+				name = n.TableName()
+			}
+			if n, ok := v.(gql.GqlDescription); ok {
+				desc = n.Description()
+			}
+			options := fmt.Sprintf(";comment on table %s is '%s';", name, desc)
+			tx := db
+			if name != "" && desc != "" {
+				tx = db.Set("gorm:table_options", options)
+			}
+			err = tx.AutoMigrate(v)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 	sqlDb, _ := db.DB()
