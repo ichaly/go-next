b40b900f102042345600a132f8064f7bb9e88b5d
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-01-22 13:45:58 +0800

    🦄 refactor: 修改字段

--- pkg/gql/types_test.go
+++ pkg/gql/types_test.go
@@ -15,7 +15,7 @@ func TestSchemaMeta(t *testing.T) {
 	b := boo{}
 	typ := reflect.TypeOf(b)
 	t.Log(reflect.TypeOf(b.parentType).Elem().Name())
-	t.Log(reflect.TypeOf(b.returnType).Elem().Name())
+	t.Log(reflect.TypeOf(b.resultType).Elem().Name())
 	if field, ok := typ.FieldByName("SchemaMeta"); ok {
 		t.Log(field.Tag.Get("name"))
 		t.Log(field.Tag.Get("description"))
