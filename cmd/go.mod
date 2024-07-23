9cc9d34590c3569180f54ca93eb0664d2c79388b
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-07-23 21:32:01 +0800

    feat: 添加graphJin侧睡用例

--- cmd/go.mod
+++ cmd/go.mod
@@ -3,7 +3,9 @@ module github.com/ichaly/go-next/main
 go 1.21.1
 
 require (
+	github.com/dosco/graphjin/core/v3 v3.0.0-20240713020306-3fb8338c363e // indirect
 	github.com/go-resty/resty/v2 v2.13.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
@@ -16,5 +18,6 @@ require (
 	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 )
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"github.com/ichaly/go-next/lib/base"
+	"context"
+	"database/sql"
+	"github.com/dosco/graphjin/core/v3"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/stretchr/testify/suite"
+	"testing"
 )
 
-type Demo struct {
-	Name        string `gorm:"size:200;comment:名字"`
-	base.Entity `mapstructure:",squash"`
+type _graphJinSuite struct {
+	suite.Suite
+	db *sql.DB
 }
 
-func (*Demo) TableName() string {
-	return "sys_demo"
+func TestGraphJin(t *testing.T) {
+	suite.Run(t, new(_graphJinSuite))
+}
+
+func (my *_graphJinSuite) SetupSuite() {
+	db, err := sql.Open("pgx", "postgres://postgres:postgres@localhost:5678/gcms?sslmode=disable")
+	my.Require().NoError(err)
+	my.db = db
+}
+
+func (my *_graphJinSuite) TestGraphJin() {
+	gj, err := core.NewGraphJin(&core.Config{}, my.db)
+	my.Require().NoError(err)
+
+	query := `{sys_user{id}sys_team{id}}`
+	ctx := context.WithValue(context.Background(), core.UserIDKey, 1)
+	res, err := gj.GraphQL(ctx, query, nil, nil)
+	my.Require().NoError(err)
+	my.T().Log(res)
 }
