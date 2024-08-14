58a0149f7e19e0344f3228d9a0c96682c82107bc
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-08-14 17:26:48 +0800

    perf: 优化表结构查询sql

--- lib/core/assets/pgsql.sql
+++ lib/core/assets/pgsql.sql
@@ -1,23 +1,47 @@
-SELECT c.table_name,
+-- pgsql一条sql实现查询所有schema为public的表名,列名,类型,是否为空,是否主键,是否外键,外键表名,外键字段,表注释,列注释信息,且在有多对多中间表是只有一条中间表记录
+SELECT t.table_name,
        c.column_name,
        c.data_type,
-       CAST(c.is_nullable AS BOOLEAN),
-       pk.is_primary,
-       fk.is_foreign,
-       obj_description(c.table_name::regclass)                     table_description,
-       col_description(c.table_name::regclass, c.ordinal_position) column_description
-FROM information_schema.columns c
-         LEFT JOIN (SELECT conrelid::regclass::text AS table_name,
-                           attnum,
-                           TRUE                     AS is_primary
-                    FROM pg_constraint
-                             JOIN pg_attribute ON conrelid = attrelid AND attnum = ANY (conkey)
-                    WHERE contype = 'p') pk ON c.table_name = pk.table_name AND c.ordinal_position = pk.attnum
-         LEFT JOIN (SELECT conrelid::regclass::text AS table_name,
-                           attnum,
-                           TRUE                     AS is_foreign
-                    FROM pg_constraint
-                             JOIN pg_attribute ON conrelid = attrelid AND attnum = ANY (conkey)
-                    WHERE contype = 'f') fk ON c.table_name = fk.table_name AND c.ordinal_position = fk.attnum
-WHERE c.table_schema = 'public'
-order by c.table_name, c.ordinal_position
+       CAST(c.is_nullable AS BOOLEAN) AS is_nullable,
+       COALESCE(pk.is_primary, false) AS is_primary,
+       COALESCE(fk.is_foreign, false) AS is_foreign,
+       fk.ref_table,
+       fk.ref_column,
+       pgd.description                AS table_description,
+       pd.description                 AS column_description
+FROM information_schema.tables t
+         JOIN
+     information_schema.columns c ON t.table_schema = c.table_schema AND t.table_name = c.table_name
+         LEFT JOIN LATERAL (
+    SELECT true AS is_primary
+    FROM information_schema.table_constraints tc
+             JOIN
+         information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
+    WHERE tc.constraint_type = 'PRIMARY KEY'
+      AND tc.table_schema = t.table_schema
+      AND tc.table_name = t.table_name
+      AND kcu.column_name = c.column_name
+    ) pk ON true
+         LEFT JOIN LATERAL (
+    SELECT true            AS is_foreign,
+           ccu.table_name  AS ref_table,
+           ccu.column_name AS ref_column
+    FROM information_schema.table_constraints tc
+             JOIN
+         information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
+             JOIN
+         information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
+    WHERE tc.constraint_type = 'FOREIGN KEY'
+      AND tc.table_schema = t.table_schema
+      AND tc.table_name = t.table_name
+      AND kcu.column_name = c.column_name
+    ) fk ON true
+         LEFT JOIN
+     pg_catalog.pg_description pgd ON pgd.objoid = (SELECT oid FROM pg_catalog.pg_class WHERE relname = t.table_name)
+         AND pgd.objsubid = 0
+         LEFT JOIN
+     pg_catalog.pg_description pd ON pd.objoid = (SELECT oid FROM pg_catalog.pg_class WHERE relname = t.table_name)
+         AND pd.objsubid = c.ordinal_position
+WHERE t.table_schema = 'public'
+ORDER BY t.table_name, c.ordinal_position;
+
