259b2401fe7def3064cb30b26e7e5e034f385775
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-08-29 23:13:55 +0800

    test: 调整测试数据

--- lib/core/assets/simple.sql
+++ lib/core/assets/simple.sql
@@ -1,48 +1,48 @@
-create table if not exists "user"
+create table if not exists "sys_user"
 (
     id bigserial
         constraint user_pk
             primary key
 );
 
-create table if not exists "area"
+create table if not exists "sys_area"
 (
     id bigserial
         constraint area_pk
             primary key
 );
 
-create table if not exists "item"
+create table if not exists "sys_item"
 (
     id      bigserial
         constraint item_pk
             primary key,
     user_id bigint
         constraint item_user_id_fk
-            references "user"
+            references "sys_user"
 );
 
-create table if not exists "team"
+create table if not exists "sys_team"
 (
     id      bigserial
         constraint team_pk
             primary key,
     pid     bigint
         constraint team_team_id_fk
-            references "team",
+            references "sys_team",
     area_id bigint
         constraint team_area_id_fk
-            references "area"
+            references "sys_area"
 );
 
-create table if not exists "edge"
+create table if not exists "sys_edge"
 (
     user_id bigint not null
         constraint edge_user_id_fk
-            references "user",
+            references "sys_user",
     team_id bigint not null
         constraint edge_team_id_fk
-            references "team",
+            references "sys_team",
     constraint edge_pk
         primary key (user_id, team_id)
 );
