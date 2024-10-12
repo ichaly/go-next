084c25b89d6ddb700c51029bb9a22c77e00898a9
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-10-12 17:50:34 +0800

    ci: 添加脚本

---
+++ git.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="i@eunut.com"
+CORRECT_NAME="shi.changliang"
+CORRECT_EMAIL="shi.changliang@zhaopin.com.cn"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+' --tag-name-filter cat -- --branches --tags
