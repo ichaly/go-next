793ad4865c22673bb7c1656c3362a03e51890287
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-10-16 13:41:36 +0800

    test: 添加条件sql编译单元测试用例

---
+++ lib/core/executor_where_test.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"context"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type _ExecutorWhereSuite struct {
+	_ExecutorSuite
+	e *Executor
+}
+
+func TestExecutorWhere(t *testing.T) {
+	suite.Run(t, new(_ExecutorWhereSuite))
+}
+
+func (my *_ExecutorWhereSuite) SetupSuite() {
+	var err error
+	my._ExecutorSuite.SetupSuite()
+
+	my.e, err = NewExecutor(my.c, my.s)
+	my.Require().NoError(err)
+}
+
+func (my *_ExecutorWhereSuite) TestExecutorWhereBase() {
+	input := "query{areaList(where:{id:{eq:1}}){id name}}"
+	expect := ""
+	r := my.e.Execute(context.Background(), input, nil)
+	my.T().Log(r.Sql)
+	my.Require().Equal(expect, r.Sql)
+}
+
+func (my *_ExecutorWhereSuite) TestExecutorWhereAnd() {
+	input := "query{areaList(where:{and:[{id:{ge:1}},{id:{le:10}}]}){id name}}"
+	expect := ""
+	r := my.e.Execute(context.Background(), input, nil)
+	my.T().Log(r.Sql)
+	my.Require().Equal(expect, r.Sql)
+}
