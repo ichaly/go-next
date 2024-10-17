385e1715b1fdb02e98b7100b293d44ecb8f592cd
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-10-17 23:00:13 +0800

    test: 添加带过滤条件的Many2Many测试用例

--- lib/core/executor_test.go
+++ lib/core/executor_test.go
@@ -117,3 +117,9 @@ func (my *_ExecutorSuite) TestExecutorWhereMany2One() {
 	expect := `SELECT jsonb_build_object('userList', __sj_0.json) AS __root FROM (SELECT true) AS __root_x LEFT OUTER JOIN LATERAL ( SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json FROM (  SELECT to_jsonb(__sr_0.*) AS json FROM (  SELECT "sys_user_0"."id" AS "key","__sj_1"."json" AS "area"FROM ( SELECT "sys_user"."id","sys_user"."area_id" FROM "sys_user" LIMIT 20 ) AS"sys_user_0" LEFT OUTER JOIN LATERAL ( SELECT COALESCE(jsonb_agg(__sj_1.json), '[]') AS json FROM (  SELECT to_jsonb(__sr_1.*) AS json FROM (  SELECT "sys_area_1"."id" AS "key"FROM ( SELECT "sys_area"."id" FROM "sys_area" WHERE (((("sys_area"."name" = '北京') AND ("sys_area"."id" = 1)) AND (("sys_area"."id") = "sys_user_0"."area_id"))) LIMIT 20 ) AS"sys_area_1" ) AS "__sr_1" ) AS "__sj_1" ) AS "__sj_1" ON true  ) AS "__sr_0" ) AS "__sj_0" ) AS "__sj_0" ON true`
 	my.doCase(input, expect)
 }
+
+func (my *_ExecutorSuite) TestExecutorWhereMany2Many() {
+	input := `query{userList{key:id teamList(where:{and:[{id:{ge:1}},{id:{le:3}}]}){key:id}}}`
+	expect := `SELECT jsonb_build_object('userList', __sj_0.json) AS __root FROM (SELECT true) AS __root_x LEFT OUTER JOIN LATERAL ( SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json FROM (  SELECT to_jsonb(__sr_0.*) AS json FROM (  SELECT "sys_user_0"."id" AS "key","__sj_1"."json" AS "area"FROM ( SELECT "sys_user"."id","sys_user"."area_id" FROM "sys_user" LIMIT 20 ) AS"sys_user_0" LEFT OUTER JOIN LATERAL ( SELECT COALESCE(jsonb_agg(__sj_1.json), '[]') AS json FROM (  SELECT to_jsonb(__sr_1.*) AS json FROM (  SELECT "sys_area_1"."id" AS "key"FROM ( SELECT "sys_area"."id" FROM "sys_area" WHERE (((("sys_area"."name" = '北京') AND ("sys_area"."id" = 1)) AND (("sys_area"."id") = "sys_user_0"."area_id"))) LIMIT 20 ) AS"sys_area_1" ) AS "__sr_1" ) AS "__sj_1" ) AS "__sj_1" ON true  ) AS "__sr_0" ) AS "__sj_0" ) AS "__sj_0" ON true`
+	my.doCase(input, expect)
+}
