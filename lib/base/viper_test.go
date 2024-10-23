afb22d535049681736ddfa76591b584fed965653
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-10-23 22:13:00 +0800

    test: 添加测试用例

--- lib/base/viper_test.go
+++ lib/base/viper_test.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"github.com/ichaly/go-next/lib/util"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -22,4 +23,34 @@ func TestNewViper(t *testing.T) {
 
 	val, err := util.MarshalJson(c)
 	t.Log(c.Name, val)
+}
+
+type BaseConfig struct {
+	Name  string `mapstructure:"name"`
+	Port  string `mapstructure:"port"`
+	Host  string `mapstructure:"host"`
+	Root  string `mapstructure:"root"`
+	Debug bool   `mapstructure:"debug"`
+}
+
+type CoreConfig struct {
+	Mapping      map[string]string `mapstructure:"mapping"`
+	UseCamel     bool              `mapstructure:"use-camel"`
+	Prefixes     []string          `mapstructure:"prefixes"`
+	BlockList    []string          `mapstructure:"block-list"`
+	DefaultLimit int               `mapstructure:"default-limit"`
+}
+
+func TestViper(t *testing.T) {
+	var b BaseConfig
+	var c CoreConfig
+	viper.SetDefault("schema.default-limit", 10)
+	viper.SetConfigFile("../../cfg/dev.yml")
+	if err := viper.ReadInConfig(); err != nil {
+		println(err)
+	}
+	if err := viper.Unmarshal(&b); err != nil {
+		return
+	}
+	println(&b, &c, viper.AllSettings())
 }
