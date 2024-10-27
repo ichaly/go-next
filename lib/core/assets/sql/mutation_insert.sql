455a043059d5d4adf18e04ff656bb6bf5077f3a1
shi.changliang <shi.changliang@zhaopin.com.cn>
2024-10-28 01:20:53 +0800

    feat: 添加更多测试用例

--- lib/core/assets/sql/mutation_insert.sql
+++ lib/core/assets/sql/mutation_insert.sql
@@ -1,11 +1,104 @@
-WITH "sys_area" AS (DELETE FROM "public"."sys_area" WHERE (("sys_area"."id") = '7') RETURNING "public"."sys_area".*)
+-- mutation{sysArea(insert:{name:"黎巴嫩" sysUser:{name:"管理员",connect:{id:{equals:1}}}}){id}}
+WITH "sys_area" AS (INSERT INTO "public"."sys_area" ("name") SELECT '黎巴嫩' :: text RETURNING "public"."sys_area".*),
+     "sys_user"
+         AS ( UPDATE "public"."sys_user" SET "area_id" = "_x_sys_area"."id" FROM "sys_area" _x_sys_area WHERE (("sys_user"."id") = '1') RETURNING "public"."sys_user".*)
 SELECT jsonb_build_object('sysArea', __sj_0.json) AS __root
 FROM ((SELECT true)) AS __root_x
          LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
                                   FROM (SELECT to_jsonb(__sr_0.*) AS json
                                         FROM (SELECT "sys_area_0"."id" AS "id"
-                                              FROM (SELECT "sys_area"."id"
-                                                    FROM "sys_area"
-                                                    WHERE (("sys_area"."id") = '7')
-                                                    LIMIT 20) AS "sys_area_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                                              FROM (SELECT "sys_area"."id" FROM "sys_area" LIMIT 20) AS "sys_area_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysArea(insert:{name:"黎巴嫩",sysUser:{name:"管理员"}}){id}}
+WITH "sys_area" AS (INSERT INTO "public"."sys_area" ("name") SELECT '黎巴嫩' :: text RETURNING "public"."sys_area".*),
+     "sys_user"
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id") SELECT '管理员' :: text, "sys_area"."id" FROM "sys_area" RETURNING "public"."sys_user".*)
+SELECT jsonb_build_object('sysArea', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_area_0"."id" AS "id"
+                                              FROM (SELECT "sys_area"."id" FROM "sys_area" LIMIT 20) AS "sys_area_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(insert:{name:"操作员",sysEdge:{userId:11,teamId:1}}){id}}
+WITH "sys_user" AS (INSERT INTO "public"."sys_user" ("name") SELECT '操作员' :: text RETURNING "public"."sys_user".*),
+     "sys_edge"
+         AS (INSERT INTO "public"."sys_edge" ("team_id", "user_id") SELECT '1' :: bigint, "sys_user"."id" FROM "sys_user" RETURNING "public"."sys_edge".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id" FROM "sys_user" LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(insert:{name:"操作员" sysTeam:{name:"北京总部"}sysEdge:{userId:11,teamId:1}}){id}}
+WITH "sys_team"
+         AS (INSERT INTO "public"."sys_team" ("name") SELECT '北京总部' :: character varying(255) RETURNING "public"."sys_team".*),
+     "sys_user"
+         AS (INSERT INTO "public"."sys_user" ("name") SELECT '操作员' :: text, "sys_team"."id" FROM "sys_team" RETURNING "public"."sys_user".*),
+     "sys_edge"
+         AS (INSERT INTO "public"."sys_edge" ("user_id", "team_id") SELECT "sys_user"."id", "sys_team"."id"
+                                                                    FROM "sys_user",
+                                                                         "sys_team" RETURNING "public"."sys_edge".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id" FROM "sys_user" LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(insert:{name:"管理员1",sysArea:{name:"伊拉克",weight:1}}where:{id:{equals:1}}){id}}
+WITH "sys_area"
+         AS (INSERT INTO "public"."sys_area" ("name", "weight") SELECT '伊拉克' :: text, '1' :: integer RETURNING "public"."sys_area".*),
+     "sys_user"
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id") SELECT '管理员1' :: text, "sys_area"."id" FROM "sys_area" RETURNING "public"."sys_user".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id"
+                                                    FROM "sys_user"
+                                                    WHERE (("sys_user"."id") = '1')
+                                                    LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(insert:{name:"管理员1" sysArea:{name:"伊拉克",weight:1,sysUser:{name:"管理员2"}}}){id}}
+WITH "sys_area"
+         AS (INSERT INTO "public"."sys_area" ("name", "weight") SELECT '伊拉克' :: text, '1' :: integer RETURNING "public"."sys_area".*),
+     sys_user_1
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id") SELECT '管理员2' :: text, "sys_area"."id" FROM "sys_area" RETURNING "public"."sys_user".*),
+     sys_user_2
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id") SELECT '管理员1' :: text, "sys_area"."id" FROM "sys_area" RETURNING "public"."sys_user".*),
+     "sys_user" AS (SELECT * FROM sys_user_1 UNION ALL SELECT * FROM sys_user_2)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id" FROM "sys_user" LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(insert:{name:"管理员1" sysArea:{name:"伊拉克" weight:1 sysUser:{name:"管理员2",sysArea:{name:"俄罗斯"}}}}){id}}
+WITH sys_area_0
+         AS (INSERT INTO "public"."sys_area" ("name", "weight") SELECT '伊拉克' :: text, '1' :: integer RETURNING "public"."sys_area".*),
+     sys_area_1 AS (INSERT INTO "public"."sys_area" ("name") SELECT '俄罗斯' :: text RETURNING "public"."sys_area".*),
+     sys_user_2
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id", "area_id") SELECT '管理员2' :: text, "sys_area"."id", "sys_area"."id"
+                                                                            FROM sys_area_0 "sys_area",
+                                                                                 sys_area_1 "sys_area" RETURNING "public"."sys_user".*),
+     sys_user_3
+         AS (INSERT INTO "public"."sys_user" ("name", "area_id") SELECT '管理员1' :: text, "sys_area"."id" FROM sys_area_0 "sys_area" RETURNING "public"."sys_user".*),
+     "sys_area" AS (SELECT * FROM sys_area_0 UNION ALL SELECT * FROM sys_area_1),
+     "sys_user" AS (SELECT * FROM sys_user_2 UNION ALL SELECT * FROM sys_user_3)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id" FROM "sys_user" LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
                          ON true
--- lib/core/assets/sql/mutation_update.sql
+++ lib/core/assets/sql/mutation_update.sql
@@ -1,9 +1,58 @@
-WITH "sys_area"
-         AS (INSERT INTO "public"."sys_area" ("name", "pid") SELECT '巴黎' :: text, '1' :: bigint RETURNING "public"."sys_area".*)
-SELECT jsonb_build_object('sysArea', __sj_0.json) AS __root
+-- mutation{sysUser(update:{name:"管理员",where:{id:{equals:1}}}){id}}
+WITH "sys_user"
+         AS (UPDATE "public"."sys_user" SET ("name") = (SELECT '管理员' :: text) WHERE (("sys_user"."id") = '1') RETURNING "public"."sys_user".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
 FROM ((SELECT true)) AS __root_x
          LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
                                   FROM (SELECT to_jsonb(__sr_0.*) AS json
-                                        FROM (SELECT "sys_area_0"."id" AS "id"
-                                              FROM (SELECT "sys_area"."id" FROM "sys_area" LIMIT 20) AS "sys_area_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
-                         ON true
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id"
+                                                    FROM "sys_user"
+                                                    WHERE (("sys_user"."id") = '1')
+                                                    LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+-- mutation{sysUser(update:{name:"管理员",sysArea:{name:"国外1",weight:1}}where:{id:{equals:1}}){id}}
+WITH "sys_user"
+         AS (UPDATE "public"."sys_user" SET ("name") = (SELECT '管理员' :: text) WHERE (("sys_user"."id") = '1') RETURNING "public"."sys_user".*),
+     "sys_area" AS (UPDATE "public"."sys_area" SET ("weight", "name", "id") =
+             (SELECT '1' :: integer, '国外1' :: text, "_x_sys_user"."area_id"
+              FROM "sys_user" _x_sys_user) FROM "sys_user" _x_sys_user WHERE (("sys_area"."id") = ("_x_sys_user"."area_id")) RETURNING "public"."sys_area".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id"
+                                                    FROM "sys_user"
+                                                    WHERE (("sys_user"."id") = '1')
+                                                    LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+
+WITH "sys_user"
+         AS (UPDATE "public"."sys_user" SET ("name") = (SELECT '管理员' :: text) WHERE (("sys_user"."id") = '1') RETURNING "public"."sys_user".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id"
+                                                    FROM "sys_user"
+                                                    WHERE (("sys_user"."id") = '1')
+                                                    LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
+
+
+WITH "sys_user"
+         AS (WITH "sys_area" AS (SELECT "id" FROM "sys_area" WHERE (("sys_area"."id") = '2') LIMIT 1) UPDATE "public"."sys_user" SET ("name") = (SELECT '管理员' :: text FROM "sys_area" _x_sys_area) WHERE (("sys_user"."id") = '1') RETURNING "public"."sys_user".*)
+SELECT jsonb_build_object('sysUser', __sj_0.json) AS __root
+FROM ((SELECT true)) AS __root_x
+         LEFT OUTER JOIN LATERAL (SELECT COALESCE(jsonb_agg(__sj_0.json), '[]') AS json
+                                  FROM (SELECT to_jsonb(__sr_0.*) AS json
+                                        FROM (SELECT "sys_user_0"."id" AS "id"
+                                              FROM (SELECT "sys_user"."id"
+                                                    FROM "sys_user"
+                                                    WHERE (("sys_user"."id") = '1')
+                                                    LIMIT 20) AS "sys_user_0") AS "__sr_0") AS "__sj_0") AS "__sj_0"
+                         ON true;
